ff39a79a7d4564c4480b5f99e2349eb430b63e86
shimaarabeay <shimaarabeaymohamed@gmail.com>
2025-11-17 18:24:34 +0200

    last_commit

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,10 +1,3 @@
 class AssetsData {
   static const String logo_bookly = 'assets/images/logo_bookly.png';
-  static const String eltafser_elmoiaser = 'assets/images/التفسير الميسر.jpg';
-  static const String elmo5taser_fe_eltafser =
-      'assets/images/المختصر في التفسير.jpg';
-  static const String ela_allah = 'assets/images/الي الله.jpg';
-  static const String resalt_yousef = 'assets/images/رسالة يوسف.jpg';
-  static const String ala_5ota_elrasoul = 'assets/images/علي خطي الرسول.webp';
-  static const String laenk_allah = 'assets/images/لانك الله.jpg';
 }
